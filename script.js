f6678dcc9447c2158c0038b750fea46879179510
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-02 17:37:14 +0300

    I done this challenge

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+ function sumTwoSmallestNums(num){
+   let sum= num.sort((a,b)=>a-b)
+    return sum[0]+sum[1]
+
+ }
+ console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])) 
+console.log(sumTwoSmallestNums([190, 300, 230, 800])) 
